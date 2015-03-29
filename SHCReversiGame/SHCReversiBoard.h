c0f1d227eb9016edd1621ea8abf012b720cce548
Giau Ngo <giaunv@gmail.com>
2015-03-30 06:13:04 +0700

    Additional Game State

---
+++ SHCReversiGame/SHCReversiBoard.h
@@ -0,0 +1,21 @@
+//
+//  SHCReversiBoard.h
+//  SHCReversiGame
+//
+//  Created by lavalamp on 3/30/15.
+//  Copyright (c) 2015 Colin Eberhardt. All rights reserved.
+//
+
+#import "SHCBoard.h"
+
+@interface SHCReversiBoard : SHCBoard
+
+// the white player's score
+@property (readonly) NSInteger whiteScore;
+
+// the black player's score
+@property (readonly) NSInteger blackScore;
+
+// sets the board to the opening positions for Reversi
+-(void) setToInitialState;
+@end
