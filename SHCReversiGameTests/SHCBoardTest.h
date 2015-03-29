d5608f4822afdd86e3bdf46750bac2b3d5c53909
Giau Ngo <giaunv@gmail.com>
2015-03-29 23:01:45 +0700

    Unit Tests

---
+++ SHCReversiGameTests/SHCBoardTest.h
@@ -0,0 +1,14 @@
+//
+//  SHCBoardTest.h
+//  SHCReversiGame
+//
+//  Created by lavalamp on 3/29/15.
+//  Copyright (c) 2015 Colin Eberhardt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface SHCBoardTest : SenTestCase
+
+@end
