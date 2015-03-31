109e8d5d69c10a54d2cbb7873bb11bbfd3bdbcf0
Giau Ngo <giaunv@gmail.com>
2015-03-31 07:53:03 +0700

    Following the Rules - Adding game logic

--- SHCReversiGame/SHCReversiBoard.h
+++ SHCReversiGame/SHCReversiBoard.h
@@ -16,6 +16,15 @@
 // the black player's score
 @property (readonly) NSInteger blackScore;
 
+// indicate the player who makes the next move
+@property (readonly) BoardCellState nextMove;
+
 // sets the board to the opening positions for Reversi
 -(void) setToInitialState;
+
+// Returns whether the player who's turn it is can make the given move
+-(bool) isValidMoveToColumn:(NSInteger)column andRow:(NSInteger)row;
+
+// Make the given move for the player who is currently taking their turn
+-(void) makeMoveToColumn:(NSInteger)column andRow:(NSInteger)row;
 @end
--- SHCReversiGame/SHCReversiBoard.m
+++ SHCReversiGame/SHCReversiBoard.m
@@ -21,5 +21,36 @@
     
     _whiteScore = 2;
     _blackScore = 2;
+    
+    // Black gets the first turn
+    _nextMove = BoardCellStateBlackPiece;
+}
+
+-(BOOL)isValidMoveToColumn:(NSInteger)column andRow:(NSInteger)row{
+    // check the cell is empty
+    if ([super cellStateAtColumn:column andRow:row] != BoardCellStateEmpty) {
+        return NO;
+    }
+    
+    return YES;
+}
+
+-(void)makeMoveToColumn:(NSInteger)column andRow:(NSInteger)row{
+    // place the playing piece at the given location
+    [self setCellState:self.nextMove forColumn:column andRow:row];
+    
+    _nextMove = [self invertState:_nextMove];
+}
+
+-(BoardCellState)invertState:(BoardCellState)state{
+    if (state == BoardCellStateBlackPiece) {
+        return BoardCellStateWhitePiece;
+    }
+    
+    if (state == BoardCellStateWhitePiece) {
+        return BoardCellStateBlackPiece;
+    }
+    
+    return BoardCellStateEmpty;
 }
 @end
